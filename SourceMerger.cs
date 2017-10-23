ff85df8e04765927a1e5d0584c8d74caf46435d0
Cero <dominikjeurissen@web.de>
2017-10-23 16:22:34 +0200

    Refactor Package Initialization

--- SourceMerger.cs
+++ SourceMerger.cs
@@ -6,82 +6,18 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Windows.Forms;
-using System.Xml;
-using System.Xml.Serialization;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace SourceMerger
 {
-    [ProvideAutoLoad("f1536ef8-92ec-443c-9ed7-fdadf150da82")]
-    internal sealed class SourceMerger
+    public sealed class SourceMerger
     {
-        public const int CommandId = 0x0100;
-        public static readonly Guid CommandSet = new Guid("fdf110a6-3b0a-4b06-b079-14fd9d3d1afd");
-
-        private readonly Package package;
-        private IServiceProvider ServiceProvider => package;
-
-        public static SourceMerger Instance{ get; private set; }
-
-        public static SourceMergerSettings Settings { get; private set; }
-
-        private SourceMerger(Package package)
-        {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
-            var commandService = ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if (commandService == null)
-                return;
-
-            var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
-            commandService.AddCommand(menuItem);
-        }
-
-        public static void Initialize(Package package)
-        {
-            var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
-            var settingsPath = Path.Combine(Path.GetDirectoryName(dte.Solution.FullName), "SourceMergerSettings.xml");
-
-            if (!File.Exists(settingsPath))
-                return;
-
-            Instance = new SourceMerger(package);
-            try
-            {
-                var xmlDocument = new XmlDocument();
-                xmlDocument.Load(settingsPath);
-                var xmlString = xmlDocument.OuterXml;
-
-                using (var stringReader = new StringReader(xmlString))
-                {
-                    var serializer = new XmlSerializer(typeof(SourceMergerSettings));
-                    using (var xmlReader = new XmlTextReader(stringReader))
-                    {
-                        Settings = (SourceMergerSettings)serializer.Deserialize(xmlReader);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                VsShellUtilities.ShowMessageBox(
-                    Instance.ServiceProvider,
-                    e.Message,
-                    "An Error occured while reading SourceMergerSettings.xml. SourceMerger will be disabled during this Session",
-                    OLEMSGICON.OLEMSGICON_WARNING,
-                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-            }
-        }
+        public SourceMergerSettings Settings { get; set; } = new SourceMergerSettings();
 
         public void MenuItemCallback(object sender, EventArgs e)
         {
@@ -97,9 +33,8 @@ namespace SourceMerger
 
             var files = Directory.GetFiles(startupPath, "*.cs", SearchOption.AllDirectories);
             var resultContent = MergeSources(files.Select(File.ReadAllText));
-
-            var settings = (SourceMergerOptionPageGrid)package.GetDialogPage(typeof(SourceMergerOptionPageGrid));
-            File.WriteAllText($@"{settings.MergePath}\{settings.MergedFileName}.cs", resultContent);
+            
+            File.WriteAllText($@"{Settings.MergeFolderPath}\{Settings.MergeFileName}.cs", resultContent);
         }
 
         public string MergeSources(IEnumerable<string> sources)
--- SourceMergerPackage.cs
+++ SourceMergerPackage.cs
@@ -13,6 +13,8 @@ using System.Globalization;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio;
@@ -20,6 +22,7 @@ using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.Win32;
+using IServiceProvider = System.IServiceProvider;
 
 namespace SourceMerger
 {
@@ -48,6 +51,10 @@ namespace SourceMerger
     public sealed class SourceMergerPackage : Package
     {
         public const string PackageGuidString = "3dbfe381-278c-44b1-a18e-e946ac39b2e6";
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("fdf110a6-3b0a-4b06-b079-14fd9d3d1afd");
+
+        public SourceMerger Instance { get; set; }
 
         private DTE2 dte;
         private Events dteEvents;
@@ -55,19 +62,67 @@ namespace SourceMerger
 
         protected override void Initialize()
         {
-            SourceMerger.Initialize(this);
             base.Initialize();
-
             //Save strong Reference to the DTE Object and Events
             dte = (DTE2)GetGlobalService(typeof(DTE));
             dteEvents = dte.Events;
             documentEvents = dteEvents.DocumentEvents;
             documentEvents.DocumentSaved += OnDocumentSaved;
+
+            Instance = new SourceMerger();
+            if (!LoadSettings())
+                return;
+            InitializeCommands();
         }
 
-        private static void OnDocumentSaved(Document document)
+        private void InitializeCommands()
         {
-            SourceMerger.Instance.MergeActiveProjectSources();
+            var commandService = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService == null)
+                return;
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(Instance.MenuItemCallback, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        private bool LoadSettings()
+        {
+            var settingsPath = Path.Combine(Path.GetDirectoryName(dte.Solution.FullName), "SourceMergerSettings.xml");
+            try
+            {
+                var xmlDocument = new XmlDocument();
+                xmlDocument.Load(settingsPath);
+                var xmlString = xmlDocument.OuterXml;
+
+                using (var stringReader = new StringReader(xmlString))
+                {
+                    var serializer = new XmlSerializer(typeof(SourceMergerSettings));
+                    using (var xmlReader = new XmlTextReader(stringReader))
+                    {
+                        Instance.Settings = (SourceMergerSettings)serializer.Deserialize(xmlReader);
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                VsShellUtilities.ShowMessageBox(
+                    this,
+                    e.Message,
+                    "An Error occured while reading SourceMergerSettings.xml. SourceMerger will be disabled during this Session",
+                    OLEMSGICON.OLEMSGICON_WARNING,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+                return false;
+            }
+        }
+
+        private void OnDocumentSaved(Document document)
+        {
+            Instance.MergeActiveProjectSources();
         }
     }
 }
